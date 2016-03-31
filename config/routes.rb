32772ebe5501f52f52efa18e7f87a6ba3cc6671d
David stapleton <stapleton.dp@gmail.com>
2016-03-31 14:15:50 +0100

    orders folder added

--- config/routes.rb
+++ config/routes.rb
@@ -60,4 +60,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+  
 end
