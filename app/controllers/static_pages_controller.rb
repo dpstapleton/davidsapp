a1f435d3a95f367a669a825de09c1185d0d1ce4a
David stapleton <stapleton.dp@gmail.com>
2016-04-07 13:25:45 +0100

    added shared folder & partials

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 class StaticPagesController < ApplicationController
   def index
   end
-  def landing_page
-    @featured_product = Product.first
-  end
+  # def landing_page
+  #   @featured_product = Product.first
+  # end
+ def landing_page
+  @products = Product.limit(3)
+end
 end
 
