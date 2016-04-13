8bf050fe4d9d76ef2009af03f9046cb279d46220
David stapleton <stapleton.dp@gmail.com>
2016-04-13 21:10:04 +0100

    email setup

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  post 'static_pages/thank_you'
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
