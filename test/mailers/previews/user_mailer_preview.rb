83a7d967a309af11234c6a515e793841d2ab7fb2
David stapleton <stapleton.dp@gmail.com>
2016-04-20 12:17:36 +0100

    action mailer

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+  def contact_form
+    UserMailer.contact_form("stapleton.dp@gmail.com", "David", "Hello World!")
+  end
+end
