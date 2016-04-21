92cd5aa1a0765714d50294afee6867951d97ef01
David stapleton <stapleton.dp@gmail.com>
2016-04-21 11:59:38 +0100

    contact page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,10 +13,10 @@ def thank_you
   @email = params[:email]
   @message = params[:message]
   UserMailer.contact_form(@email, @name, @message).deliver_now
-  # ActionMailer::Base.mail(:from => @email,
-  #     :to => 'stapleton.dp@gmail.com',
-  #     :subject => "A new contact form message from #{@name}",
-  #     :body => @message).deliver_now
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'stapleton.dp@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
 end
 
 
