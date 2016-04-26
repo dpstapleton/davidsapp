0525fa1b4f33270bec5e600afc6dba2ec15c4ef2
David stapleton <stapleton.dp@gmail.com>
2016-04-26 15:17:08 +0100

    bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin_user = User.new( :first_name => Admin :email => admin@admin.com, :password => admin12345 , :password_confirmation => admin12345 )
-admin_user.admin = true
-admin_user.save!
+# admin_user = User.new( :first_name => Admin :email => admin@admin.com, :password => admin12345 , :password_confirmation => admin12345 )
+# admin_user.admin = true
+# admin_user.save!
