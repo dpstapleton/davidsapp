7ce7fa5df12672bbf0936f45d2b87396b7d6018f
David stapleton <stapleton.dp@gmail.com>
2016-06-07 15:26:50 +0100

    ajax comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,13 +3,12 @@ $(document).on('ready page:load', function(){
 	$('.img-zoom').elevateZoom({
 		zoomType	: "inner", cursor: "crosshair"
 
-	});
-	
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+	}); 
+
+
+$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty( { path: '/assets',
+    readOnly: true,
+    score: function() { return $(this).attr('data-score');}
+  });
 });
